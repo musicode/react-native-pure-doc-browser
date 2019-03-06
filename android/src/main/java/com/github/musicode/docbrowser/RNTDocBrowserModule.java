e4fc5e3f251b63b503537364737bfa8317516499
musicode <musicode@qq.com>
2019-03-06 17:20:22 +0800

    update

--- android/src/main/java/com/github/musicode/docbrowser/RNTDocBrowserModule.java
+++ android/src/main/java/com/github/musicode/docbrowser/RNTDocBrowserModule.java
@@ -3,6 +3,8 @@ package com.github.musicode.docbrowser;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -34,16 +36,27 @@ public class RNTDocBrowserModule extends ReactContextBaseJavaModule {
         File file = new File(path);
         String mimeType = URLConnection.guessContentTypeFromName(file.getName());
 
-        if (mimeType != null && !mimeType.isEmpty()) {
-            Intent intent = new Intent("android.intent.action.VIEW");
-            intent.addCategory("android.intent.category.DEFAULT");
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            Uri uri = Uri.fromFile(file);
-            intent.setDataAndType(uri, mimeType);
-            Activity activity = reactContext.getCurrentActivity();
-            if (activity != null) {
-                activity.startActivity(intent);
+        Activity activity = reactContext.getCurrentActivity();
+        if (activity != null && mimeType != null && !mimeType.isEmpty()) {
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+
+            // https://developer.android.com/reference/android/support/v4/content/FileProvider.html
+            // rb-fetch-blob 实现了一个 fileprovider，直接用它的
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                Uri uri = FileProvider.getUriForFile(activity, getReactApplicationContext().getPackageName() + ".provider", file);
+                intent.setDataAndType(uri, mimeType);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             }
+            else {
+                Uri uri = Uri.fromFile(file);
+                intent.setDataAndType(uri, mimeType);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+
+            activity.startActivity(intent);
+
         }
 
     }
